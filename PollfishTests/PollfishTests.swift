86bae42fe5ad193a6c1181f82af62cc4d6396f95
Gaurang <gaurang@ayopop.com>
2020-01-28 10:22:51 +0530

    Initial Commit

---
+++ PollfishTests/PollfishTests.swift
@@ -0,0 +1,34 @@
+//
+//  PollfishTests.swift
+//  PollfishTests
+//
+//  Created by Ayopop on 28/01/20.
+//  Copyright © 2020 Pollfish. All rights reserved.
+//
+
+import XCTest
+@testable import Pollfish
+
+class PollfishTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
